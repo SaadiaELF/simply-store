a599c7fd0861870d3eb728ab38601a8e7b67b569
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 21:20:12 +0100

    Added AddProduct function

--- components/grid/grid.js
+++ components/grid/grid.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import GridItem from "../gridItem/gridItem";
 import styles from "./grid.module.css";
 
-export default function Grid() {
+export default function Grid({ setSelectedProducts, selectedProducts }) {
   const [products, setProducts] = useState([]);
+
   async function getAllProducts() {
     const request = await fetch(
       "https://mock.shop/api?query={products(first:%2030){edges%20{node%20{id%20title%20description%20featuredImage%20{url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}"
@@ -12,9 +13,20 @@ export default function Grid() {
     const productsData = response.data.products.edges;
     setProducts(productsData);
   }
+  
   useEffect(() => {
     getAllProducts();
   }, []);
+
+  function AddProduct(item) {
+    setSelectedProducts(
+      selectedProducts.concat({
+        title: item.node.title,
+        imageUrl: item.node.featuredImage.url,
+        price: parseInt(item.node.variants.edges[0].node.price.amount),
+      })
+    );
+  }
 
   return (
     <div className={styles.grid}>
@@ -26,6 +38,7 @@ export default function Grid() {
             description={item.node.description}
             price={item.node.variants.edges[0].node.price.amount}
             key={i}
+            onClick={() => AddProduct(item)}
           ></GridItem>
         );
       })}
