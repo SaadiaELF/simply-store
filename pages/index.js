18ad698ee831a56d38573e116ac361ba3d93e9c5
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 21:21:57 +0100

    Added stectedProducts state

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Nav from "../components/nav/nav";
 import Main from "../components/main/main";
 
 export default function Home() {
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
   return (
     <>
-      <Nav />
-      <Main />
+      <Nav selectedProducts={selectedProducts} />
+      <Main
+        selectedProducts={selectedProducts}
+        setSelectedProducts={setSelectedProducts}
+      />
     </>
   );
 }
