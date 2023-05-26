ff0d65465405dbce9575209fdfb6897f46b8f0fd
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 01:20:06 +0100

    moved head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="author" content="Saadia El fekak" />
+        <meta name="description" content="Simply Fashion Store"></meta>
+        <title>Simply.</title>
+      </Head>{" "}
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
