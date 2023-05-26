4f7c5cb5a844179877a58d1ce69332d3b4a8444c
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 14:39:18 +0100

    formating

--- components/gridItem/gridItem.js
+++ components/gridItem/gridItem.js
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import styles from "./gridItem.module.css";
+import formatNum from "../../utils/formatNum";
 
 export default function GridItem({ imageUrl, title, description, price }) {
   return (
     <div className={styles.container}>
       <img className={styles.img} src={imageUrl} alt={title} />
-      <h2 className={styles.title}>{title}</h2>
+      <h3 className={styles.title}>{title}</h3>
       <div className={styles.wrapper}>
-        <h3 className={styles.description}>{"£" + price}</h3>
+        <h2 className={styles.description}>{formatNum(price)}</h2>
         <button>Add to Basket</button>
       </div>
       {/* <p className={styles.description}>{description}</p> */}
