dbefee128927a98ed845774b4a402e57efc57218
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 21:19:30 +0100

    Added Count

--- components/basket/basket.js
+++ components/basket/basket.js
@@ -1,12 +1,18 @@
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
 import styles from "./basket.module.css";
 
-export default function Basket() {
+export default function Basket({ selectedProducts }) {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setCount(selectedProducts.length.toString());
+  }, [selectedProducts]);
+
   return (
     <div className={styles.container}>
       <FontAwesomeIcon icon={faBasketShopping} size="xl" />
-      <p className={styles.quantity}>0</p>
+      <p className={styles.quantity}>{count}</p>
     </div>
   );
 }
