fc573143ab123545e419e9c751882aac3e4ed479
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 14:38:47 +0100

    Added basket

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,5 +1,6 @@
 import NavItem from "../navItem/navItem";
 import Header from "../header/header";
+import Basket from "../basket/basket";
 import styles from "./nav.module.css";
 
 export default function Nav() {
@@ -12,6 +13,7 @@ export default function Nav() {
         {navItems.map((item, i) => {
           return <NavItem key={i} href={item.href} name={item.name} />;
         })}
+        <Basket />
       </div>
     </nav>
   );
