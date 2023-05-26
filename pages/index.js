b21123110bb2aa12c99c2a345ec64b653f69681d
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 02:27:40 +0100

    Adde main

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Nav from "../components/nav/nav";
+import Main from "../components/main/main";
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <Main />
     </>
   );
 }
