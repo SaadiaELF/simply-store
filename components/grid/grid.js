f0acfd02090a6240dfe243d9061d79d174cf2ba1
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-26 14:40:44 +0100

    updated api call

--- components/grid/grid.js
+++ components/grid/grid.js
@@ -6,7 +6,7 @@ export default function Grid() {
   const [products, setProducts] = useState([]);
   async function getAllProducts() {
     const request = await fetch(
-      "https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}"
+      "https://mock.shop/api?query={products(first:%2030){edges%20{node%20{id%20title%20description%20featuredImage%20{url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}"
     );
     const response = await request.json();
     const productsData = response.data.products.edges;
