7b256af195bd2264b36c9892da3c758258236fd1
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-28 12:51:17 +0100

    refactoring

--- components/header/header.js
+++ components/header/header.js
@@ -4,7 +4,7 @@ export default function Header({ title, subtitle }) {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>{title}</h1>
-      <h2 className={styles.subtitle}>{subtitle}</h2>
+      {subtitle && <h2 className={styles.subtitle}>{subtitle}</h2>}
     </header>
   );
 }
