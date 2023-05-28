ed2433bc69636b2856546ba0338462a8f36c1e2b
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-28 21:58:11 +0100

    Added spin loader

--- components/loading/loading.js
+++ components/loading/loading.js
@@ -1,5 +1,5 @@
 import styles from "./loading.module.css";
 
 export default function Loading() {
-  return <h2 className={styles.text}>Loading ...</h2>;
+  return <div className={styles.loader}></div>;
 }
